50aabe34788a25982732e4cbe576224702c72764
panyuewh <worm_hole5565@hotmail.com>
2024-06-05 18:20:49 +0800

    Till Logic Equivalence

--- Logic.v
+++ Logic.v
@@ -143,7 +143,14 @@ Qed.
 Example and_exercise :
   forall n m : nat, n + m = 0 -> n = 0 /\ m = 0.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n m. split.
+  - (* n = 0 *) destruct m as [| m'].
+    + (* m = 0 *) rewrite add_comm in H. simpl in H. apply H.
+    + (* m = S m' *) rewrite add_comm in H. simpl in H. discriminate H.
+  - (* m = 0 *) destruct n as [| n'].
+    + (* n = 0 *) simpl in H. apply H.
+    + (* n = S n' *) simpl in H. discriminate H. 
+Qed. 
 (** [] *)
 
 (** So much for proving conjunctive statements.  To go in the other
@@ -222,7 +229,9 @@ Proof.
 Lemma proj2 : forall P Q : Prop,
   P /\ Q -> Q.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros P Q [_ HQ].
+  apply HQ. Qed.
+
 (** [] *)
 
 (** Finally, we sometimes need to rearrange the order of conjunctions
@@ -248,7 +257,11 @@ Theorem and_assoc : forall P Q R : Prop,
   P /\ (Q /\ R) -> (P /\ Q) /\ R.
 Proof.
   intros P Q R [HP [HQ HR]].
-  (* FILL IN HERE *) Admitted.
+  split. split.
+  - apply HP.
+  - apply HQ.
+  - apply HR.
+Qed. 
 (** [] *)
 
 (** Finally, the infix notation [/\] is actually just syntactic sugar for
@@ -312,14 +325,22 @@ Qed.
 Lemma mult_is_O :
   forall n m, n * m = 0 -> n = 0 \/ m = 0.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n m H. destruct n as [| n'].
+  - left. reflexivity.
+  - destruct m as [| m'].
+    + right. reflexivity.
+    + discriminate H.
+Qed.
 (** [] *)
 
 (** **** Exercise: 1 star, standard (or_commut) *)
 Theorem or_commut : forall P Q : Prop,
   P \/ Q  -> Q \/ P.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros P Q [HP | HQ].
+  - right. apply HP.
+  - left. apply HQ.
+Qed.
 (** [] *)
 
 (* ================================================================= *)
@@ -378,7 +399,9 @@ Proof.
 Theorem not_implies_our_not : forall (P:Prop),
   ~ P -> (forall (Q:Prop), P -> Q).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros P HNP Q HP. unfold not in HNP. 
+  apply HNP in HP. destruct HP. Qed.
+
 (** [] *)
 
 (** Inequality is a very common form of negated statement, so there is a
@@ -447,14 +470,16 @@ Definition manual_grade_for_double_neg_inf : option (nat*string) := None.
 Theorem contrapositive : forall (P Q : Prop),
   (P -> Q) -> (~Q -> ~P).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros P Q HPQ. unfold not. intros HQF. intro HP.
+  apply HQF. apply HPQ. apply HP. Qed.
 (** [] *)
 
 (** **** Exercise: 1 star, standard (not_both_true_and_false) *)
 Theorem not_both_true_and_false : forall P : Prop,
   ~ (P /\ ~P).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros P. unfold not. intros [HP HPF].
+  apply HPF. apply HP. Qed.
 (** [] *)
 
 (** **** Exercise: 1 star, advanced (informal_not_PNP)
@@ -479,7 +504,10 @@ Definition manual_grade_for_informal_not_PNP : option (nat*string) := None.
 Theorem de_morgan_not_or : forall (P Q : Prop),
     ~ (P \/ Q) -> ~P /\ ~Q.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros P Q. unfold not. intros HPQ. split.
+  - intro HP. apply HPQ. left. apply HP.
+  - intro HQ. apply HPQ. right. apply HQ.
+Qed. 
 (** [] *)
 
 (** Since inequality involves a negation, it also requires a little
